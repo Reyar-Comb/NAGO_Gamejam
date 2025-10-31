56d0d4434940930130817666feb05f9145f6138b
Ghogm4 <30169665+Ghogm4@users.noreply.github.com>
2025-10-31 20:49:53 +0800

    Created basic player movement and changed project settings

---
+++ Player/Player.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	[Export] public float Speed = 300.0f;
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		Vector2 direction = Input.GetVector("Left", "Right", "Up", "Down");
+		if (direction != Vector2.Zero)
+			velocity = direction * Speed;
+		else
+			velocity = Vector2.Zero;
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
